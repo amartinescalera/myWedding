842a8c51946d678ce2f303ca3ed9d1b9a6b15847
Antonio Martin <antoniomartinescalera@gmail.com>
2024-04-01 17:56:34 +0200

    first commit

---
+++ myWedding/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package newlyweds
+ */
+
+get_header(); ?>
+
+Donde quedarse
+Como llegar
+	<div id="primary" class="content-area sidebar">
+		<main id="main" class="site-main">
+			Hello it's me
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+//get_sidebar();
+get_footer();
